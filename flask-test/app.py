da5eea6632de7107c4b2a9bd28dd775dad50c925
Colin Delorme <rexdelorian@gmailcom>
2018-01-18 11:12:18 -0500

    Commit to port code from aiohttp to flask

---
+++ flask-test/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import render_template, request
+# from flask_bootstrap import Bootstrap
+
+# def create_app():
+#    app = Flask(__name__)
+#    Bootstrap(app)
+
+#    return app
+
+# app = create_app()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("/index.html")
+
+
+@app.route('/connect', methods=["GET", "POST"])
+def connect():
+    if request.method == 'POST':
+        return render_template("connect.html")
+
+    return render_template("connect.html")
+
+
+if __name__ == "__main__":
+    app.run()
