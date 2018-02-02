33e62aa15ef90d5706441f4c25e57951447bc01d
DESKTOP-PS9VTEF\phant <dcbarbosa15@gmail.com>
2018-02-02 00:03:20 -0500

    Daniel's Card and Deck classes

---
+++ deck.py
@@ -0,0 +1,25 @@
+class Deck(object):
+	def __init__(self):
+		self.cards = []
+		for suit in range(4):
+			for rank in range(1, 14):
+				card = Card(suit, rank)
+				self.cards.append(card)
+				
+	def __str__(self):
+		res = []
+		for card in self.cards:
+			res.append(str(card))
+		return '\n'.join(res)
+		
+	def pop_card(self, i=-1):
+		return self.cards.pop(i)
+		
+	def shuffle(self):
+		random.shuffle(self.cards)
+		
+	def sort(self):
+		self.cards.sort()
+	
+	def numCards(self):
+		return len(self.cards)
