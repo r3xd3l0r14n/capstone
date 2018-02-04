881e203218357c1bbe32a707a4d55d8721367de7
Jason Gray <grayjn@gmail.com>
2018-02-04 18:45:33 +0000

    updating player with card, deck and hand updates.

--- player.py
+++ player.py
@@ -1,9 +1,26 @@
 class Player:
 
-    def __init__(self, player_id, name):
+    def __init__(self, player_id, name, deck):
         self._id = player_id
         self.name = name
+        self.hand = Hand(deck) #need to discuss, we may need to pass the deck back and forth via JSON
+		self.book = []
+		self.deck = deck
+		self.score = 0
 
+    def draw(self):
+		cardDrawn = self.deck.pop_card()
+		self.hand.addCard(cardDrawn)
+		print('%s drew %s.' % (self.name, cardDrawn))
+
+    def numCards(self):
+		return self.hand.numCards()
+		
+	def printHand(self):
+		return self.hand.printHand()
+		
+	def checkHand(self, rank):
+		return self.hand.checkCards(rank)
 
     def toString(self):
         return self.name, self._id
