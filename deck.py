729fc224a762ecf19a0f0b1ca3c33c728fa4df1e
Jason Gray <grayjn@gmail.com>
2018-02-04 18:41:25 +0000

    initial deck check in

---
+++ deck.py
@@ -0,0 +1,30 @@
+class Deck:
+
+    def __init__(self):
+		self.cards = []
+		for suit in range(4):
+			for rank in range(1, 14):
+				card = Card(suit, rank)
+				self.cards.append(card)
+				
+	def __str__(self):
+		res = []
+		for card in self.cards:
+			res.append(str(card))
+		return '\n'.join(res)
+		
+	def pop_card(self, i=-1):
+		return self.cards.pop(i)
+		
+	def shuffle(self):
+		random.shuffle(self.cards)
+		
+	def sort(self):
+		self.cards.sort()
+	
+	def move_cards(self, hand, num):
+		for i in range(num):
+			hand.add_card(self.pop_card())
+			
+	def numCards(self):
+		return len(self.cards)
